f0ce1f06afe3d2a9e1015bf78046ede7000239aa
ANAM <khoirul130203@gmail.com>
2025-09-18 21:38:57 +0700

    update main layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,19 @@
 import { Stack } from "expo-router";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import Toast from "react-native-toast-message";
 
 export default function RootLayout() {
   return (
-    <Stack>
+    <SafeAreaProvider>
+      <Stack>
       <Stack.Screen name="index" options={{ title:'Index' }}/>
       <Stack.Screen name="register" options={{ title:'Register' }}/>
       <Stack.Screen name="login" options={{ title:'Login' }}/>
       <Stack.Screen name="about" options={{ title:'About' }}/>
       <Stack.Screen name="home" options={{ title:'Home' }}/>
+
+      <Toast/>
     </Stack>
+    </SafeAreaProvider>
   );
 }
